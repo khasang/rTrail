d6fbbdbd6d416170616f6d986eb7c8e3b9928800
skuznet <azon.sk@gmail.com>
2018-05-24 20:11:59 +0300

    RT-0002 add spring security

--- src/main/java/io/khasang/rtrail/controller/AppController.java
+++ src/main/java/io/khasang/rtrail/controller/AppController.java
@@ -1,12 +1,15 @@
 package io.khasang.rtrail.controller;
 
+import io.khasang.rtrail.model.CreateTable;
 import io.khasang.rtrail.model.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 // http://localhost:8080
@@ -15,10 +18,32 @@ public class AppController {
     @Qualifier("main")
     private Message message;
 
+    @Autowired
+    private CreateTable createTable;
+
     @RequestMapping("/")
     public String getHelloPage(Model model) {
         model.addAttribute("name", message.getInfo());
         return "index";
+    }
+
+    @RequestMapping("/create")
+    public String getCreateTableStatus(Model model) {
+        model.addAttribute("status", createTable.createTableStatus());
+        return "create";
+    }
+
+    @RequestMapping(value = "/get/{name}", method = RequestMethod.GET)
+    public String getCatByName(@PathVariable("name") String name, Model model) {
+        model.addAttribute("info", createTable.getCatByName(name));
+        return "info";
+    }
+
+    @RequestMapping(value = "/password/{password}", method = RequestMethod.GET)
+    public String getPasswordByEncode(@PathVariable("password") String password, Model model) {
+        model.addAttribute("password", password);
+        model.addAttribute("encodePassword", new BCryptPasswordEncoder().encode(password));
+        return "password";
     }
 
 }
---
+++ src/main/java/io/khasang/rtrail/model/CreateTable.java
@@ -0,0 +1,70 @@
+package io.khasang.rtrail.model;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CreateTable {
+    private JdbcTemplate jdbcTemplate;
+
+    public CreateTable(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public CreateTable() {
+    }
+
+    public String createTableStatus() {
+        try {
+            // open
+            jdbcTemplate.execute("DROP TABLE IF EXISTS cats");
+            // close
+
+            // open
+            jdbcTemplate.execute("CREATE TABLE public.cats\n" +
+                    "(\n" +
+                    "  id integer NOT NULL,\n" +
+                    "  name character varying(255),\n" +
+                    "  description character varying(255),\n" +
+                    "  color_id integer,\n" +
+                    "  CONSTRAINT cats_pkey PRIMARY KEY (id)\n" +
+                    ")");
+            return "table created";
+        } catch (Exception e) {
+            return "Table creation failed: " + e;
+        }
+    }
+
+    public String getCatByName(String name) {
+//        String query = "Select * from cats where name  = ?";
+//        return jdbcTemplate.execute(query, new PreparedStatementCallback<String>() {
+//            @Override
+//            public String doInPreparedStatement(PreparedStatement ps) throws SQLException, DataAccessException {
+//                ps.setString(1, name);
+//                return String.valueOf(ps.execute());
+//            }
+//        });
+        return String.valueOf(jdbcTemplate.query("select * from cats where name = 'Barsik'", new RowMapper<Cat>() {
+            @Override
+            public Cat mapRow(ResultSet rs, int rownumber) throws SQLException {
+                Cat e = new Cat();
+                e.setId(rs.getInt(1));
+                e.setName(rs.getString(2));
+                e.setDescription(rs.getString(3));
+                e.setColorId(rs.getInt(4));
+                return e;
+            }
+        }));
+
+    }
+
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
+    }
+
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+}
