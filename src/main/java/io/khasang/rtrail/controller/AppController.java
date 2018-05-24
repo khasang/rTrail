0d928f612e642fc870af13bd357aa9fafc5dad45
al2mes <igormed505@gmail.com>
2018-05-24 11:10:32 +0500

    RT-0001 add basic proj structure

--- src/main/java/io/khasang/rtrail/controller/AppController.java
+++ src/main/java/io/khasang/rtrail/controller/AppController.java
@@ -1,10 +1,14 @@
 package io.khasang.rtrail.controller;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Controller
+@ComponentScan("io.khasang.rtrail")
+@EnableWebMvc
 // http://localhost:8080
 public class AppController {
 
--- web/WEB-INF/views/index.jsp
+++ web/WEB-INF/views/index.jsp
@@ -1,9 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: al2mes
+  Date: 24.05.2018
+  Time: 11:04
+  To change this template use File | Settings | File Templates.
+--%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Title</title>
+    <title>${name}</title>
 </head>
 <body>
-    <h1>Hello ${name}</h1>
+    <h1>Hello, ${name}</h1>
 </body>
 </html>
